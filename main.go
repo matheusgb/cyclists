2119bac6570593fa315f6fa102bd9c8f0559a8bc
matheusgb <matheusgbr98@gmail.com>
2024-01-10 17:47:59 -0300

    feat: initializes connection to database in main

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 	routes "github.com/matheusgb/cyclists/api"
 	"github.com/matheusgb/cyclists/config"
+	"github.com/matheusgb/cyclists/database"
 )
 
 func main() {
 	config := config.Init()
+	database.CreateGormDatabase().VerifyConnection(config)
 
 	app := fiber.New()
 	v1 := app.Group("/api/v1")
