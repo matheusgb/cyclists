e04d6b4c0fb7bc6b9ea7701762c9528bade75590
matheusgb <matheusgbr98@gmail.com>
2024-01-10 12:50:47 -0300

    feat: create User model

---
+++ api/model/User.go
@@ -0,0 +1,10 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"not null"`
+	Password string `gorm:"not null"`
+}
