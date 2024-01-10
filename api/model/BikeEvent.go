5d9a09cf237239d0ac06ec1bfb85c43d74662a19
matheusgb <matheusgbr98@gmail.com>
2024-01-10 12:51:37 -0300

    refactor: remove json tags from BikeEvent model

--- api/model/BikeEvent.go
+++ api/model/BikeEvent.go
@@ -4,13 +4,13 @@ import "gorm.io/gorm"
 
 type BikeEvent struct {
 	gorm.Model
-	Name                  string  `gorm:"not null" json:"name"`
-	StartDate             string  `gorm:"not null" json:"start_date"`
-	StartDateRegistration string  `gorm:"not null" json:"start_date_registration"`
-	EndDateRegistration   string  `gorm:"not null" json:"end_date_registration"`
-	AditionalInformation  *string `json:"aditional_information"`
-	StartPlace            string  `gorm:"not null" json:"start_place"`
-	ParticipantsLimit     *uint   `json:"participants_limit"`
-	Organizer             uint    `gorm:"not null" json:"organizer"`
-	User                  User    `gorm:"foreignKey:Organizer"`
+	Name                  string `gorm:"not null"`
+	StartDate             string `gorm:"not null"`
+	StartDateRegistration string `gorm:"not null"`
+	EndDateRegistration   string `gorm:"not null"`
+	AditionalInformation  *string
+	StartPlace            string `gorm:"not null"`
+	ParticipantsLimit     *uint
+	Organizer             uint `gorm:"not null"`
+	User                  User `gorm:"foreignKey:Organizer"`
 }
