734814548ee0ee8fabe3001c6a10f82802cd9d41
matheusgb <matheusgbr98@gmail.com>
2024-01-10 13:00:13 -0300

    feat: create EventSubscription model

---
+++ api/models/EventSubscription.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type EventSubscription struct {
+	EventID          uint      `gorm:"not null"`
+	BikeEvent        BikeEvent `gorm:"foreignKey:ID"`
+	UserID           uint      `gorm:"not null"`
+	User             User      `gorm:"foreignKey:ID"`
+	SubscriptionDate time.Time `gorm:"not null"`
+}
