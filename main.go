cc546eb8f33097b1cfdd39e29eab851d9a48b547
matheusgb <matheusgbr98@gmail.com>
2024-01-11 14:29:39 -0300

    refactor: adjust appointments in main function

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	routes "github.com/matheusgb/cyclists/api"
 	"github.com/matheusgb/cyclists/config"
-	"github.com/matheusgb/cyclists/database"
+	"github.com/matheusgb/cyclists/gorm"
 )
 
 func main() {
 	config := config.Init()
-	database.CreateGormDatabase().VerifyConnection(config)
+	gorm.CreateDatabase().VerifyConnection(config)
 
 	app := fiber.New()
 	v1 := app.Group("/api/v1")
-	routes.BikeEvents(v1)
+	BikeEvents(v1)
 
 	app.Listen(config.Api.Port)
 }
