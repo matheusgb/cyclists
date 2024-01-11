5a044ed7604c5c18ea43164c7a2207ca42bddb21
matheusgb <matheusgbr98@gmail.com>
2024-01-11 17:01:35 -0300

    refactor: adjusts BikeEvent typing

--- api/models/BikeEvent.go
+++ api/models/BikeEvent.go
@@ -1,18 +1,20 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type BikeEvent struct {
 	gorm.Model
-	Name                  string  `gorm:"not null" json:"name"`
-	StartDate             string  `gorm:"not null" json:"start_date"`
-	StartDateRegistration string  `gorm:"not null" json:"start_date_registration"`
-	EndDateRegistration   string  `gorm:"not null" json:"end_date_registration"`
-	AditionalInformation  *string `json:"aditional_information"`
-	StartPlace            string  `gorm:"not null" json:"start_place"`
-	ParticipantsLimit     *uint   `json:"participants_limit"`
-	Organizer             uint    `gorm:"not null" json:"organizer"`
-	User                  User    `gorm:"foreignKey:Organizer"`
+	Name                  string    `gorm:"not null" json:"name"`
+	StartDate             time.Time `gorm:"not null" json:"start_date"`
+	StartDateRegistration time.Time `gorm:"not null" json:"start_date_registration"`
+	EndDateRegistration   time.Time `gorm:"not null" json:"end_date_registration"`
+	AditionalInformation  *string   `json:"aditional_information"`
+	StartPlace            string    `gorm:"not null" json:"start_place"`
+	ParticipantsLimit     *uint     `json:"participants_limit"`
+	Organizer             uint      `gorm:"not null" json:"organizer"`
+	User                  User      `gorm:"foreignKey:Organizer"`
 }
