a9c7f142a6488f7b8dbd17bd93a8dad3a353654a
matheusgb <matheusgbr98@gmail.com>
2024-01-11 21:41:53 -0300

    refactor: creates a file only for things related to connecting to the database

---
+++ gorm/connection.go
@@ -0,0 +1,30 @@
+package gorm
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/matheusgb/cyclists/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func (gormDB *GormDatabase) Connect(config config.Config) {
+	if gormDB.client == nil {
+		gormDB.InitializeClient(config)
+	} else {
+		log.Fatal("Database connection already established")
+	}
+}
+
+func (gormDB *GormDatabase) InitializeClient(config config.Config) {
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", config.Database.User, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Name)
+
+	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to open client connection")
+	}
+
+	gormDB.client = client
+	log.Println("Client initialized")
+}
