8af0b1a5f19d1996cdfb484b5b0e56aa82743751
matheusgb <matheusgbr98@gmail.com>
2024-01-11 21:41:08 -0300

    refactor: creates the IDatabase interface to improve code consistency

--- gorm/database.go
+++ gorm/database.go
@@ -1,46 +1,20 @@
 package gorm
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/matheusgb/cyclists/config"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+type IDatabase interface {
+	Connect(config config.Config)
+	InitializeClient(config config.Config)
+	RunMigrations(config config.Config)
+}
 
 type GormDatabase struct {
 	client *gorm.DB
 }
 
-func Database() *GormDatabase {
+func Init() IDatabase {
 	return &GormDatabase{}
-}
-
-func (db *GormDatabase) VerifyConnection(config config.Config) *gorm.DB {
-	var client *gorm.DB
-	var err error
-
-	if db.client == nil {
-		client, err = db.Connect(config)
-		if err != nil {
-			log.Fatal("Connection attempt failed: ", err)
-		}
-	}
-
-	return client
-}
-
-func (db *GormDatabase) Connect(config config.Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s", config.Database.User, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Name)
-
-	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Connection attempt failed: ", err)
-		return nil, err
-	}
-
-	log.Println("Database connected")
-	db.client = client
-	return client, nil
 }
