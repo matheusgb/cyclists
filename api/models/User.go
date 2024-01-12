2de77e973d797b82f685163040da4adeb0629502
matheusgb <matheusgbr98@gmail.com>
2024-01-11 21:39:31 -0300

    refactor: rearranges and removes json from User model

--- api/models/User.go
+++ api/models/User.go
@@ -4,15 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name                 string `gorm:"not null" json:"name"`
-	Email                string `gorm:"not null" json:"email"`
-	Password             string `gorm:"not null" json:"password"`
-	PasswordConfirmation string `gorm:"-" json:"password_confirmation"`
-}
-
-type omit *struct{}
-
-type UserResponse struct {
-	*User
-	Password omit `json:"password,omitempty"`
+	Name                 string `gorm:"not null"`
+	Email                string `gorm:"not null"`
+	Password             string `gorm:"not null"`
+	PasswordConfirmation string `gorm:"-" `
+	Deleted              bool   `gorm:"not null" `
 }
