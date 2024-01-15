a9465747cac146f4319d26c3dc64cda9c9d4f527
matheusgb <matheusgbr98@gmail.com>
2024-01-15 18:17:12 -0300

    fix: fixes type in Init function, User struct and CreateUser return in interface

--- src/models/services/userService.go
+++ src/models/services/userService.go
@@ -1,15 +1,19 @@
 package services
 
-import "github.com/matheusgb/cyclists/src/models/repositories"
+import (
+	"github.com/matheusgb/cyclists/src/models/domains"
+	"github.com/matheusgb/cyclists/src/models/repositories"
+	"github.com/matheusgb/cyclists/src/views"
+)
 
 type IUser interface {
-	// CreateUser(user domains.User) (domains.User, error)
+	CreateUser(user domains.User) (views.UserResponse, error)
 }
 
 type User struct {
-	repository repositories.User
+	repository repositories.IUser
 }
 
-func Init(service repositories.User) IUser {
+func Init(service repositories.IUser) IUser {
 	return &User{service}
 }
