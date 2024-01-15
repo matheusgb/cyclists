e1104fcc54283e794493b3561810fb55f75e7316
matheusgb <matheusgbr98@gmail.com>
2024-01-15 18:22:14 -0300

    feat: create struct for request

---
+++ src/controllers/requests/createUser.go
@@ -0,0 +1,8 @@
+package requests
+
+type UserCreate struct {
+	Name                 string `json:"name"`
+	Email                string `json:"email"`
+	Password             string `json:"password"`
+	PasswordConfirmation string `json:"password_confirmation"`
+}
