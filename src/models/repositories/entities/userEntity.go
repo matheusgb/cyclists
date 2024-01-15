8de11ffd330ef27f5a48f402ab033ff7023d6bf7
matheusgb <matheusgbr98@gmail.com>
2024-01-15 16:25:27 -0300

    refactor: remove passwordConfirmation from User entity

--- src/models/repositories/entities/userEntity.go
+++ src/models/repositories/entities/userEntity.go
@@ -1,12 +1,13 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Name                 string `gorm:"not null"`
-	Email                string `gorm:"not null"`
-	Password             string `gorm:"not null"`
-	PasswordConfirmation string `gorm:"-" `
-	Deleted              bool   `gorm:"not null" `
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"not null"`
+	Password string `gorm:"not null"`
+	Deleted  bool   `gorm:"not null"`
 }
