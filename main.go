f3e10858fc37332ff21e179ee387dedfa8b60133
matheusgb <matheusgbr98@gmail.com>
2024-01-15 18:24:34 -0300

    feat: create initialization of MVC layers

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/matheusgb/cyclists/config"
 	gorm "github.com/matheusgb/cyclists/database"
+	controllers "github.com/matheusgb/cyclists/src/controllers/user"
+	"github.com/matheusgb/cyclists/src/models/repositories"
+	"github.com/matheusgb/cyclists/src/models/services"
 )
 
 func main() {
@@ -11,10 +14,13 @@ func main() {
 	database := gorm.Init()
 	database.Connect(config)
 	database.RunMigrations(config)
+	repository := repositories.Init(database.GetClient())
+	service := services.Init(repository)
+	controller := controllers.Init(service)
 
 	app := fiber.New()
 	v1 := app.Group("/api/v1")
-	UserRoutes(v1)
+	UserRoutes(v1, controller)
 
 	app.Listen(config.Api.Port)
 }
