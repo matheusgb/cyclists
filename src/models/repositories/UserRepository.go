ab58fafa5b1c6bd4ce726e68d23200ef588b837c
matheusgb <matheusgbr98@gmail.com>
2024-01-15 18:19:06 -0300

    refactor: rename Init function and adjust return in interface

--- src/models/repositories/UserRepository.go
+++ src/models/repositories/UserRepository.go
@@ -1,17 +1,19 @@
 package repositories
 
 import (
+	"github.com/matheusgb/cyclists/src/models/domains"
+	"github.com/matheusgb/cyclists/src/views"
 	"gorm.io/gorm"
 )
 
 type IUser interface {
-	// CreateUser(user domains.UserDomain) entities.User
+	CreateUser(user domains.User) (views.UserResponse, error)
 }
 
 type User struct {
 	database *gorm.DB
 }
 
-func InitUserRepository(database *gorm.DB) IUser {
+func Init(database *gorm.DB) IUser {
 	return &User{database}
 }
