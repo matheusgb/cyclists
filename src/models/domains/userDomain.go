5f9755b000fbbebd779c12a234c9d9ab7e1a4db3
matheusgb <matheusgbr98@gmail.com>
2024-01-15 16:29:18 -0300

    feat: create User domain initialization

--- src/models/domains/userDomain.go
+++ src/models/domains/userDomain.go
@@ -1,1 +1,18 @@
 package domains
+
+type User struct {
+	Name                 string
+	Email                string
+	Password             string
+	PasswordConfirmation string
+}
+
+// inicializar no controller com os valores da request
+func Init(email, password, passwordConfirmation, name string) *User {
+	return &User{
+		Email:                email,
+		Password:             password,
+		PasswordConfirmation: passwordConfirmation,
+		Name:                 name,
+	}
+}
