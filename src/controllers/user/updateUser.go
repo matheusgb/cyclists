16b999a794e6726e1442102a510c8b2cdeb1ff90
matheusgb <matheusgbr98@gmail.com>
2024-01-16 20:01:40 -0300

    feat: create UpdateUser controller

---
+++ src/controllers/user/updateUser.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/matheusgb/cyclists/src/controllers/requests"
+	"github.com/matheusgb/cyclists/src/models/domains"
+	"github.com/matheusgb/cyclists/src/views"
+)
+
+func (user *User) UpdateUser(ctx *fiber.Ctx) error {
+	var request requests.UpdateUser
+	UserID := ctx.Params("id", "")
+
+	if err := ctx.BodyParser(&request); err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+		})
+		return err
+	}
+
+	domain := domains.InitUpdate(request.Name, UserID)
+
+	service, err := user.service.UpdateUser(*domain)
+	if err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+		return err
+	}
+
+	view := views.ConvertUserEntityToResponse(&service)
+	ctx.Status(201).JSON(view)
+	return nil
+}
