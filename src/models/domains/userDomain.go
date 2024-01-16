79e206dc8eee9d11660f89b32fef0395929fd073
matheusgb <matheusgbr98@gmail.com>
2024-01-16 20:04:05 -0300

    feat: create user update domain initialization and adjust InitCreate name

--- src/models/domains/userDomain.go
+++ src/models/domains/userDomain.go
@@ -1,18 +1,25 @@
 package domains
 
 type User struct {
+	ID                   string
 	Name                 string
 	Email                string
 	Password             string
 	PasswordConfirmation string
 }
 
-// inicializar no controller com os valores da request
-func Init(email, password, passwordConfirmation, name string) *User {
+func InitCreate(email, password, passwordConfirmation, name string) *User {
 	return &User{
 		Email:                email,
 		Password:             password,
 		PasswordConfirmation: passwordConfirmation,
 		Name:                 name,
+	}
+}
+
+func InitUpdate(name, id string) *User {
+	return &User{
+		ID:   id,
+		Name: name,
 	}
 }
