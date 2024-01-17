5cd62a6abf76a7a3096ed9e6b90768ce8d98e79c
matheusgb <matheusgbr98@gmail.com>
2024-01-16 21:24:20 -0300

    fix: fixes error handling on UpdateUser repository

--- src/models/repositories/updateUser.go
+++ src/models/repositories/updateUser.go
@@ -1,6 +1,8 @@
 package repositories
 
 import (
+	"fmt"
+
 	"github.com/matheusgb/cyclists/src/models/domains"
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
 )
@@ -8,12 +10,11 @@ import (
 func (user *User) UpdateUser(domain domains.User) (entities.User, error) {
 	var entity entities.User
 
-	err := user.database.Model(&entities.User{}).Where("id = ?", domain.ID).Updates(entities.User{
+	result := user.database.Where("id = ?", domain.ID).Updates(&entities.User{
 		Name: domain.Name,
-	}).Scan(&entity).Error
-
-	if err != nil {
-		return entity, err
+	}).Scan(&entity)
+	if result.RowsAffected == 0 {
+		return entity, fmt.Errorf("User with id %s not found", domain.ID)
 	}
 
 	return entity, nil
