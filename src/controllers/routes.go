4daa87ed2e5af5c65ceb0849ab7d910937e60b5f
matheusgb <matheusgbr98@gmail.com>
2024-01-18 17:27:58 -0300

    feat: create BikeEvent routes initializer

--- src/controllers/routes.go
+++ src/controllers/routes.go
@@ -2,14 +2,20 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
-	controllers "github.com/matheusgb/cyclists/src/controllers/user"
+	Bcontrollers "github.com/matheusgb/cyclists/src/controllers/bikeEvent"
+	Ucontrollers "github.com/matheusgb/cyclists/src/controllers/user"
 )
 
-func UserRoutes(app fiber.Router, controller controllers.IUser) {
+func UserRoutes(app fiber.Router, controller Ucontrollers.IUser) {
 	User := app.Group("/user")
 	User.Post("/", controller.CreateUser)
 	User.Get("/", controller.GetAllUsers)
 	User.Get("/:id", controller.GetUser)
 	User.Put("/:id", controller.UpdateUser)
 	User.Delete("/:id", controller.DeleteUser)
+}
+
+func BikeEventRoutes(app fiber.Router, controller Bcontrollers.IBikeEvent) {
+	User := app.Group("/bike-event")
+	User.Post("/", controller.CreateBikeEvent)
 }
