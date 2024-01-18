8be62a643bdaf0d501b2ca1ce5ed8e3a1cb12080
matheusgb <matheusgbr98@gmail.com>
2024-01-18 17:24:41 -0300

    feat: create CreateBikeEvent controller function

---
+++ src/controllers/bikeEvent/createBikeEvent.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	requests "github.com/matheusgb/cyclists/src/controllers/requests/bikeEvent"
+	domains "github.com/matheusgb/cyclists/src/models/domains/bikeEvent"
+	views "github.com/matheusgb/cyclists/src/views/bikeEvent"
+)
+
+func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
+	var request requests.CreateBikeEvent
+
+	if err := ctx.BodyParser(&request); err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+		})
+		return err
+	}
+
+	domain := domains.InitCreate(request.Name, request.StartPlace, request.AditionalInformation, request.StartDate, request.StartDateRegistration, request.EndDateRegistration, request.Organizer, request.ParticipantsLimit)
+
+	entity, err := bikeEvent.service.CreateBikeEvent(*domain)
+	if err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+		return nil
+	}
+
+	view := views.ConvertUserEntityToResponse(&entity)
+	ctx.Status(201).JSON(view)
+	return nil
+}
