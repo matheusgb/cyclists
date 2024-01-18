64ad46ba9750b56fc5808caadd77026527beeb68
matheusgb <matheusgbr98@gmail.com>
2024-01-18 17:26:19 -0300

    feat: create CreateBikeEvent request struct

---
+++ src/controllers/requests/bikeEvent/createBikeEvent.go
@@ -0,0 +1,14 @@
+package requests
+
+import "time"
+
+type CreateBikeEvent struct {
+	Name                  string    `json:"name"`
+	StartPlace            string    `json:"start_place"`
+	StartDate             time.Time `json:"start_date"`
+	StartDateRegistration time.Time `json:"start_date_registration"`
+	EndDateRegistration   time.Time `json:"end_date_registration"`
+	Organizer             uint      `json:"organizer"`
+	ParticipantsLimit     *uint     `json:"participants_limit"`
+	AditionalInformation  *string   `json:"aditional_information"`
+}
