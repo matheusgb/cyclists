73efe89a48bc672016a7aedf0f4093a76e7b83e7
matheusgb <matheusgbr98@gmail.com>
2024-01-17 21:42:51 -0300

    feat: create GetAllUsers controller function

---
+++ src/controllers/user/getAllUsers.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/matheusgb/cyclists/src/views"
+)
+
+func (user *User) GetAllUsers(ctx *fiber.Ctx) error {
+	entities, err := user.service.GetAllUsers()
+	if err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+		return nil
+	}
+
+	view := views.ConvertAllUsersEntityToResponse(entities)
+	ctx.Status(200).JSON(view)
+	return nil
+}
