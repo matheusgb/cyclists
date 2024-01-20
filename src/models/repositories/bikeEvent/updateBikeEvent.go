4419e5f3df096b3267177a6acf374d6af0467c7c
matheusgb <matheusgbr98@gmail.com>
2024-01-20 18:13:43 -0300

    feat: create UpdateBikeEvent function in BikeEvent repository

---
+++ src/models/repositories/bikeEvent/updateBikeEvent.go
@@ -0,0 +1,31 @@
+package repositories
+
+import (
+	"fmt"
+
+	domains "github.com/matheusgb/cyclists/src/models/domains/bikeEvent"
+	"github.com/matheusgb/cyclists/src/models/repositories/entities"
+)
+
+func (bikeEvent *BikeEvent) UpdateBikeEvent(domain domains.BikeEvent) (entities.BikeEvent, error) {
+	var entity entities.BikeEvent
+
+	result := bikeEvent.database.Where("id = ?", domain.ID).Updates(&entities.BikeEvent{
+		Name:                  domain.Name,
+		StartPlace:            domain.StartPlace,
+		AditionalInformation:  domain.AditionalInformation,
+		StartDate:             domain.StartDate,
+		StartDateRegistration: domain.StartDateRegistration,
+		EndDateRegistration:   domain.EndDateRegistration,
+		ParticipantsLimit:     domain.ParticipantsLimit,
+	}).Scan(&entity)
+	if result.Error != nil {
+		return entity, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return entity, fmt.Errorf("bike event with id %s not found", domain.ID)
+	}
+
+	return entity, nil
+}
