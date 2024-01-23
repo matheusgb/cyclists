18117a37e1f6902f91e005ed27ef93b612be47f5
matheusgb <matheusgbr98@gmail.com>
2024-01-23 20:23:04 -0300

    refactor: adjust GetUser service return

--- src/models/services/user/getUser.go
+++ src/models/services/user/getUser.go
@@ -6,9 +6,5 @@ import (
 )
 
 func (user *User) GetUser(domain domains.User) (entities.User, error) {
-	entity, err := user.repository.GetUser(domain)
-	if err != nil {
-		return entity, err
-	}
-	return entity, nil
+	return user.repository.GetUser(domain)
 }
