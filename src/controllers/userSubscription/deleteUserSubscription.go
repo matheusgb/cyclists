2008fd7efa87d7a0d7ca8601fc90f84a55d70ae9
matheusgb <matheusgbr98@gmail.com>
2024-01-26 16:51:49 -0300

    feat: create DeleteUserSubscription function controller

---
+++ src/controllers/userSubscription/deleteUserSubscription.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
+)
+
+func (UserSubscription *UserSubscription) DeleteUserSubscription(ctx *fiber.Ctx) error {
+	userSubscriptionID := ctx.Params("id", "")
+	if userSubscriptionID == "" {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": "Invalid request",
+		})
+	}
+
+	domain := domains.InitID(userSubscriptionID)
+
+	_, err := UserSubscription.service.DeleteUserSubscription(*domain)
+	if err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+		return nil
+	}
+
+	ctx.Status(204)
+	return nil
+}
