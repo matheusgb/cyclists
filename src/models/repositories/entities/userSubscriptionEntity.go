1818b9a390fcaa2a4744ff95908aca267203fdbc
matheusgb <matheusgbr98@gmail.com>
2024-01-25 21:03:14 -0300

    feat: create UserSubscription entity struct

---
+++ src/models/repositories/entities/userSubscriptionEntity.go
@@ -0,0 +1,17 @@
+package entities
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type UserSubscription struct {
+	ID          uint      `gorm:"primarykey"`
+	BikeEventID uint      `gorm:"not null"`
+	BikeEvent   BikeEvent `gorm:"foreignKey:BikeEventID"`
+	UserID      uint      `gorm:"not null"`
+	User        User      `gorm:"foreignKey:UserID"`
+	CreatedAt   time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
