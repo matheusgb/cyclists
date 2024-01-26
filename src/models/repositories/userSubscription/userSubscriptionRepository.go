b8e4b0795d85067a730d0d337f6fcad821042fce
matheusgb <matheusgbr98@gmail.com>
2024-01-25 21:03:55 -0300

    feat: create IUserSubscription repository interface

---
+++ src/models/repositories/userSubscription/userSubscriptionRepository.go
@@ -0,0 +1,19 @@
+package repositories
+
+import (
+	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
+	"github.com/matheusgb/cyclists/src/models/repositories/entities"
+	"gorm.io/gorm"
+)
+
+type IUserSubscription interface {
+	CreateUserSubscription(userSubscription domains.UserSubscription) (entities.UserSubscription, error)
+}
+
+type UserSubscription struct {
+	database *gorm.DB
+}
+
+func Init(database *gorm.DB) IUserSubscription {
+	return &UserSubscription{database}
+}
