d8f85846b54a809731b67315efb1ce7146c3342c
matheusgb <matheusgbr98@gmail.com>
2024-01-25 21:01:43 -0300

    feat: create IUserSubscription controller interface

---
+++ src/controllers/userSubscription/userSubscriptionController.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	services "github.com/matheusgb/cyclists/src/models/services/userSubscription"
+)
+
+type UserSubscription struct {
+	service services.IUserSubscription
+}
+
+type IUserSubscription interface {
+	CreateUserSubscription(ctx *fiber.Ctx) error
+}
+
+func Init(service services.IUserSubscription) IUserSubscription {
+	return &UserSubscription{service}
+}
