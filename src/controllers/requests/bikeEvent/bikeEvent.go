2cf24f0b16bf28b314a7f51c162cb3f735fce8ac
matheusgb <matheusgbr98@gmail.com>
2024-01-27 20:04:52 -0300

    refactor: adjust BikeEvent request struct

--- src/controllers/requests/bikeEvent/bikeEvent.go
+++ src/controllers/requests/bikeEvent/bikeEvent.go
@@ -2,12 +2,13 @@ package requests
 
 import "time"
 
-type UpdateBikeEvent struct {
+type BikeEvent struct {
 	Name                  string    `json:"name"`
 	StartPlace            string    `json:"start_place"`
 	StartDate             time.Time `json:"start_date"`
 	StartDateRegistration time.Time `json:"start_date_registration"`
 	EndDateRegistration   time.Time `json:"end_date_registration"`
+	Organizer             uint      `json:"organizer"`
 	ParticipantsLimit     *uint     `json:"participants_limit"`
 	AditionalInformation  *string   `json:"aditional_information"`
 }
