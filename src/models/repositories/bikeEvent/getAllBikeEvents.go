684e9e4a600fb05aad9ab559425d0865f32e5193
matheusgb <matheusgbr98@gmail.com>
2024-01-27 20:07:17 -0300

    refactor: use table relationship in GetAllBikeEvents function

--- src/models/repositories/bikeEvent/getAllBikeEvents.go
+++ src/models/repositories/bikeEvent/getAllBikeEvents.go
@@ -9,7 +9,7 @@ import (
 func (bikeEvent *BikeEvent) GetAllBikeEvents() ([]entities.BikeEvent, error) {
 	var entities []entities.BikeEvent
 
-	result := bikeEvent.database.Select("id, name, start_date, start_date_registration, end_date_registration, aditional_information, start_place, participants_limit, organizer, created_at, updated_at").Find(&entities)
+	result := bikeEvent.database.Joins("User").Find(&entities)
 	if result.Error != nil {
 		return entities, result.Error
 	}
