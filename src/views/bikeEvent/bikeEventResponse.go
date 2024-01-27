a8ef73a345b8f66d0dba414a6179c6d31f266622
matheusgb <matheusgbr98@gmail.com>
2024-01-27 20:09:52 -0300

    refactor: change Organizer in BikeEventResponse struct to views.UserResponse

--- src/views/bikeEvent/bikeEventResponse.go
+++ src/views/bikeEvent/bikeEventResponse.go
@@ -4,20 +4,21 @@ import (
 	"time"
 
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
+	views "github.com/matheusgb/cyclists/src/views/user"
 )
 
 type BikeEventResponse struct {
-	ID                    uint      `json:"id"`
-	Name                  string    `json:"name"`
-	StartPlace            string    `json:"start_place"`
-	StartDate             time.Time `json:"start_date"`
-	StartDateRegistration time.Time `json:"start_date_registration"`
-	EndDateRegistration   time.Time `json:"end_date_registration"`
-	Organizer             uint      `json:"organizer"`
-	ParticipantsLimit     *uint     `json:"participants_limit"`
-	AditionalInformation  *string   `json:"aditional_information"`
-	CreatedAt             time.Time `json:"created_at"`
-	UpdatedAt             time.Time `json:"updated_at"`
+	ID                    uint               `json:"id"`
+	Name                  string             `json:"name"`
+	StartPlace            string             `json:"start_place"`
+	StartDate             time.Time          `json:"start_date"`
+	StartDateRegistration time.Time          `json:"start_date_registration"`
+	EndDateRegistration   time.Time          `json:"end_date_registration"`
+	Organizer             views.UserResponse `json:"organizer"`
+	ParticipantsLimit     *uint              `json:"participants_limit"`
+	AditionalInformation  *string            `json:"aditional_information"`
+	CreatedAt             time.Time          `json:"created_at"`
+	UpdatedAt             time.Time          `json:"updated_at"`
 }
 
 func ConvertBikeEventEntityToResponse(bikeEventEntity *entities.BikeEvent) *BikeEventResponse {
@@ -28,7 +29,7 @@ func ConvertBikeEventEntityToResponse(bikeEventEntity *entities.BikeEvent) *Bike
 		StartDate:             bikeEventEntity.StartDate,
 		StartDateRegistration: bikeEventEntity.StartDateRegistration,
 		EndDateRegistration:   bikeEventEntity.EndDateRegistration,
-		Organizer:             bikeEventEntity.Organizer,
+		Organizer:             *views.ConvertUserEntityToResponse(&bikeEventEntity.User),
 		ParticipantsLimit:     bikeEventEntity.ParticipantsLimit,
 		AditionalInformation:  bikeEventEntity.AditionalInformation,
 		CreatedAt:             bikeEventEntity.CreatedAt,
