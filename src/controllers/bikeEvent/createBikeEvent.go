0019c3ae4109d67bd058617eec8cae7e5d478154
matheusgb <matheusgbr98@gmail.com>
2024-01-27 20:03:31 -0300

    refactor: remove CreateBikeEvent response

--- src/controllers/bikeEvent/createBikeEvent.go
+++ src/controllers/bikeEvent/createBikeEvent.go
@@ -4,11 +4,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 	requests "github.com/matheusgb/cyclists/src/controllers/requests/bikeEvent"
 	domains "github.com/matheusgb/cyclists/src/models/domains/bikeEvent"
-	views "github.com/matheusgb/cyclists/src/views/bikeEvent"
 )
 
 func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
-	var request requests.CreateBikeEvent
+	var request requests.BikeEvent
 
 	if err := ctx.BodyParser(&request); err != nil {
 		ctx.Status(400).JSON(fiber.Map{
@@ -19,7 +18,7 @@ func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
 
 	domain := domains.InitCreate(request.Name, request.StartPlace, request.AditionalInformation, request.StartDate, request.StartDateRegistration, request.EndDateRegistration, request.Organizer, request.ParticipantsLimit)
 
-	entity, err := bikeEvent.service.CreateBikeEvent(*domain)
+	_, err := bikeEvent.service.CreateBikeEvent(*domain)
 	if err != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": err.Error(),
@@ -27,7 +26,6 @@ func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
 		return nil
 	}
 
-	view := views.ConvertBikeEventEntityToResponse(&entity)
-	ctx.Status(201).JSON(view)
+	ctx.Status(201)
 	return nil
 }
