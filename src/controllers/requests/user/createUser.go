04944812d6d43e54ba5be55daff624eebb2f6c13
matheusgb <matheusgbr98@gmail.com>
2024-01-28 21:39:34 -0300

    feat: add validators in CreateUser request struct

--- src/controllers/requests/user/createUser.go
+++ src/controllers/requests/user/createUser.go
@@ -1,8 +1,8 @@
 package requests
 
 type CreateUser struct {
-	Name                 string `json:"name"`
-	Email                string `json:"email"`
-	Password             string `json:"password"`
-	PasswordConfirmation string `json:"password_confirmation"`
+	Name                 string `json:"name" validate:"required,min=3,max=255"`
+	Email                string `json:"email" validate:"required,email"`
+	Password             string `json:"password" validate:"required"`
+	PasswordConfirmation string `json:"password_confirmation" validate:"required,eqfield=Password"`
 }
