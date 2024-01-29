b68a002382bd666aaff22550ece4642b745570bb
matheusgb <matheusgbr98@gmail.com>
2024-01-28 21:40:49 -0300

    refactor: adjust error order in DeleteUser repository

--- src/models/repositories/user/deleteUser.go
+++ src/models/repositories/user/deleteUser.go
@@ -11,11 +11,11 @@ func (user *User) DeleteUser(domain domains.User) (entities.User, error) {
 	var entity entities.User
 
 	result := user.database.Where("id = ?", domain.ID).Delete(&entity)
-	if result.Error != nil {
-		return entity, result.Error
-	}
 	if result.RowsAffected == 0 {
 		return entity, fmt.Errorf("user with id %s not found", domain.ID)
+	}
+	if result.Error != nil {
+		return entity, result.Error
 	}
 
 	return entity, nil
