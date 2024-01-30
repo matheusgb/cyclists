9e0e16ae1a2d9e2bcd7364526fd0d36f2a5bc114
matheusgb <matheusgbr98@gmail.com>
2024-01-29 22:20:37 -0300

    feat: add new validations on requests.CreateUser struct

--- src/controllers/requests/user/createUser.go
+++ src/controllers/requests/user/createUser.go
@@ -3,6 +3,6 @@ package requests
 type CreateUser struct {
 	Name                 string `json:"name" validate:"required,min=3,max=255"`
 	Email                string `json:"email" validate:"required,email"`
-	Password             string `json:"password" validate:"required"`
+	Password             string `json:"password" validate:"required,min=6,max=255"`
 	PasswordConfirmation string `json:"password_confirmation" validate:"required,eqfield=Password"`
 }
