108fca278c24b37a8124650ac6c11f67916f980f
matheusgb <matheusgbr98@gmail.com>
2024-01-30 21:13:00 -0300

    feat: add validator in CreateBikeEvent controller function

--- src/controllers/bikeEvent/createBikeEvent.go
+++ src/controllers/bikeEvent/createBikeEvent.go
@@ -4,16 +4,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 	requests "github.com/matheusgb/cyclists/src/controllers/requests/bikeEvent"
 	domains "github.com/matheusgb/cyclists/src/models/domains/bikeEvent"
+	"github.com/matheusgb/cyclists/validator"
 )
 
 func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
-	var request requests.BikeEvent
+	var request requests.CreateBikeEvent
 
 	if err := ctx.BodyParser(&request); err != nil {
 		ctx.Status(400).JSON(fiber.Map{
-			"message": "Invalid request",
+			"message": err.Error(),
 		})
-		return err
+		return nil
+	}
+
+	errValidator := validator.BikeEvent[requests.CreateBikeEvent](request)
+	if errValidator != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": errValidator,
+		})
+		return nil
 	}
 
 	domain := domains.InitCreate(request.Name, request.StartPlace, request.AditionalInformation, request.StartDate, request.StartDateRegistration, request.EndDateRegistration, request.Organizer, request.ParticipantsLimit)
