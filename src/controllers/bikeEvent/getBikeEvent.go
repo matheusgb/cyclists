8c307b46af2240c3722746528cc9b45f1c4b013f
matheusgb <matheusgbr98@gmail.com>
2024-01-30 21:14:27 -0300

    refactor: adjust GetBikeEvent controller error message

--- src/controllers/bikeEvent/getBikeEvent.go
+++ src/controllers/bikeEvent/getBikeEvent.go
@@ -10,8 +10,9 @@ func (bikeEvent *BikeEvent) GetBikeEvent(ctx *fiber.Ctx) error {
 	bikeEventID := ctx.Params("id", "")
 	if bikeEventID == "" {
 		ctx.Status(400).JSON(fiber.Map{
-			"message": "Invalid request",
+			"message": "BikeEvent ID is required",
 		})
+		return nil
 	}
 
 	domain := domains.InitID(bikeEventID)
