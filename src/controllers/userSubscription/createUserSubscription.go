c7b3077e223a9a454f84b0775ce13f56d9902a6c
matheusgb <matheusgbr98@gmail.com>
2024-01-30 21:51:33 -0300

    feat: add validator in CreateUserSubscription controller

--- src/controllers/userSubscription/createUserSubscription.go
+++ src/controllers/userSubscription/createUserSubscription.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	requests "github.com/matheusgb/cyclists/src/controllers/requests/userSubscription"
 	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
+	"github.com/matheusgb/cyclists/validator"
 )
 
 func (UserSubscription *UserSubscription) CreateUserSubscription(ctx *fiber.Ctx) error {
@@ -12,6 +13,14 @@ func (UserSubscription *UserSubscription) CreateUserSubscription(ctx *fiber.Ctx)
 	if err := ctx.BodyParser(&request); err != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": err.Error(),
+		})
+		return nil
+	}
+
+	errorsValidation := validator.UserSubscription(request)
+	if errorsValidation != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"errors": errorsValidation,
 		})
 		return nil
 	}
