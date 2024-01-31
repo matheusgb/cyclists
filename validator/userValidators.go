9d421edbb2acf4d2ca30742f315e776a2311bf3c
matheusgb <matheusgbr98@gmail.com>
2024-01-30 21:18:56 -0300

    refactor: use generics in User validator

--- validator/userValidators.go
+++ validator/userValidators.go
@@ -5,33 +5,20 @@ import (
 	requests "github.com/matheusgb/cyclists/src/controllers/requests/user"
 )
 
-var mapCreateUserRequestErrors = map[string]string{
+var mapUserRequestErrors = map[string]string{
 	"name":                  "Invalid name, it must have at least 3 characters and at most 255 characters",
 	"email":                 "Invalid email",
 	"password":              "Invalid password, it must have at least 6 characters and at most 255 characters",
 	"password_confirmation": "Invalid password confirmation, it must be equal to password",
 }
 
-func CreateUser(request requests.CreateUser) map[string]string {
+func User[requestUser requests.CreateUser | requests.UpdateUser](request requestUser) map[string]string {
 	err := Validator.Struct(request)
 	if err != nil {
 		errors := make(map[string]string)
 		for _, error := range err.(validator.ValidationErrors) {
 			field := error.Field()
-			errors[field] = mapCreateUserRequestErrors[field]
-		}
-		return errors
-	}
-	return nil
-}
-
-func UpdateUser(request requests.UpdateUser) map[string]string {
-	err := Validator.Struct(request)
-	if err != nil {
-		errors := make(map[string]string)
-		for _, error := range err.(validator.ValidationErrors) {
-			field := error.Field()
-			errors[field] = mapCreateUserRequestErrors[field]
+			errors[field] = mapUserRequestErrors[field]
 		}
 		return errors
 	}
