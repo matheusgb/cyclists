e0e05d1200a506e77bd8c246678071800d04bce7
matheusgb <matheusgbr98@gmail.com>
2024-02-02 20:51:17 -0300

    refactor: improve CreateUser service errors

--- src/models/services/user/createUser.go
+++ src/models/services/user/createUser.go
@@ -11,6 +11,8 @@ func (user *User) CreateUser(domain domains.User) (entities.User, error) {
 	userFound, err := user.repository.FindUserByEmail(domain)
 	if err == nil {
 		return userFound, fmt.Errorf("user already exists")
+	} else if err.Error() != "record not found" {
+		return userFound, err
 	}
 
 	return user.repository.CreateUser(domain)
