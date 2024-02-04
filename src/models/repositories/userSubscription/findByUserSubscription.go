4de62d4838215f436c5d646e7215326d50ad53fb
matheusgb <matheusgbr98@gmail.com>
2024-02-04 17:37:38 -0300

    feat: adjust FindByUserSubscription validation and use CheckEventIsFull validation

--- src/models/repositories/userSubscription/findByUserSubscription.go
+++ src/models/repositories/userSubscription/findByUserSubscription.go
@@ -1,17 +1,18 @@
 package repositories
 
 import (
+	"fmt"
+
 	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
 )
 
-func (userSubscription *UserSubscription) FindByUserSubscription(domain domains.UserSubscription) (entities.UserSubscription, error) {
-	var entity entities.UserSubscription
-
-	err := userSubscription.database.Where(domain).First(&entity).Error
-	if err != nil {
-		return entity, err
+func (userSubscription *UserSubscription) FindByUserSubscription(domain domains.UserSubscription) error {
+	err := userSubscription.database.Where(domain).First(&entities.UserSubscription{}).Error
+	if err == nil {
+		return fmt.Errorf("user already subscribed to this event")
+	} else if err.Error() != "record not found" {
+		return err
 	}
-
-	return entity, nil
+	return nil
 }
--- src/models/services/userSubscription/createUserSubscription.go
+++ src/models/services/userSubscription/createUserSubscription.go
@@ -1,18 +1,19 @@
 package services
 
 import (
-	"fmt"
-
 	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
 )
 
 func (userSubscription *UserSubscription) CreateUserSubscription(domain domains.UserSubscription) (entities.UserSubscription, error) {
-	subscriptrionFound, err := userSubscription.repository.FindByUserSubscription(domain)
-	if err == nil {
-		return subscriptrionFound, fmt.Errorf("user already subscribed to this event")
-	} else if err.Error() != "record not found" {
-		return subscriptrionFound, err
+	err := userSubscription.repository.FindByUserSubscription(domain)
+	if err != nil {
+		return entities.UserSubscription{}, err
+	}
+
+	err = userSubscription.repository.CheckEventIsFull(domain)
+	if err != nil {
+		return entities.UserSubscription{}, err
 	}
 
 	return userSubscription.repository.CreateUserSubscription(domain)
