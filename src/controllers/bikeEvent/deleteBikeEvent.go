968782924024f485b065b84cf9f253501a71be2a
matheusgb <matheusgbr98@gmail.com>
2024-04-02 17:57:51 -0300

    feat: add user and role validation in DeleteBikeEvent controller

--- src/controllers/bikeEvent/deleteBikeEvent.go
+++ src/controllers/bikeEvent/deleteBikeEvent.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	domains "github.com/matheusgb/cyclists/src/models/domains/bikeEvent"
 )
@@ -9,14 +11,24 @@ func (bikeEvent *BikeEvent) DeleteBikeEvent(ctx *fiber.Ctx) error {
 	bikeEventID := ctx.Params("id", "")
 	if bikeEventID == "" {
 		ctx.Status(400).JSON(fiber.Map{
-			"message": "BikeEvent ID is required",
+			"message": "bike event ID is required",
 		})
 		return nil
 	}
 
-	domain := domains.InitID(bikeEventID)
+	userRole := ctx.Locals("user_role").(string)
+	uintOrganizerId, err := strconv.ParseUint(ctx.Locals("user_id").(string), 10, 64)
+	if err != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"message": "invalid context user ID",
+		})
+	}
+	organizerId := uint(uintOrganizerId)
 
-	_, err := bikeEvent.service.DeleteBikeEvent(*domain)
+	domain := domains.InitID(bikeEventID)
+	domain.Organizer = organizerId
+
+	_, err = bikeEvent.service.DeleteBikeEvent(*domain, userRole)
 	if err != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": err.Error(),
