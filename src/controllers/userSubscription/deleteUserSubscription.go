f676eefe106fa2c8ecee0c4af93d8ef9e500b045
matheusgb <matheusgbr98@gmail.com>
2024-04-03 18:44:50 -0300

    feat: create user validator in DeleteUserSubscription and change endpoint body

--- src/controllers/userSubscription/deleteUserSubscription.go
+++ src/controllers/userSubscription/deleteUserSubscription.go
@@ -1,22 +1,51 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	requests "github.com/matheusgb/cyclists/src/controllers/requests/userSubscription"
+	"github.com/matheusgb/cyclists/src/controllers/validators"
 	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
 )
 
 func (UserSubscription *UserSubscription) DeleteUserSubscription(ctx *fiber.Ctx) error {
-	userSubscriptionID := ctx.Params("id", "")
-	if userSubscriptionID == "" {
+	var request requests.UserSubscription
+
+	if err := ctx.BodyParser(&request); err != nil {
 		ctx.Status(400).JSON(fiber.Map{
-			"message": "userSubscription ID is required",
+			"message": err.Error(),
 		})
 		return nil
 	}
 
-	domain := domains.InitID(userSubscriptionID)
+	errorsValidation := validators.UserSubscription(request)
+	if errorsValidation != nil {
+		ctx.Status(400).JSON(fiber.Map{
+			"errors": errorsValidation,
+		})
+		return nil
+	}
 
-	_, err := UserSubscription.service.DeleteUserSubscription(*domain)
+	userRole := ctx.Locals("user_role").(string)
+	intUserId, err := strconv.ParseUint(ctx.Locals("user_id").(string), 10, 64)
+	if err != nil {
+		ctx.Status(500).JSON(fiber.Map{
+			"message": "internal error",
+		})
+	}
+	userId := uint(intUserId)
+
+	if request.UserID != userId && userRole != "admin" {
+		ctx.Status(403).JSON(fiber.Map{
+			"message": "you don't have permission to delete this user subscription",
+		})
+		return nil
+	}
+
+	domain := domains.Init(request.BikeEventID, request.UserID)
+
+	_, err = UserSubscription.service.DeleteUserSubscription(*domain)
 	if err != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": err.Error(),
