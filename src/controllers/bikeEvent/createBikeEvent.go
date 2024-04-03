114dc4424f2525339381beffbd44c594427062fe
matheusgb <matheusgbr98@gmail.com>
2024-04-03 18:41:15 -0300

    feat: create user validation in CreateBikeEvent

--- src/controllers/bikeEvent/createBikeEvent.go
+++ src/controllers/bikeEvent/createBikeEvent.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	requests "github.com/matheusgb/cyclists/src/controllers/requests/bikeEvent"
 	"github.com/matheusgb/cyclists/src/controllers/validators"
@@ -17,7 +19,7 @@ func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
 		return nil
 	}
 
-	errValidator := validators.BikeEvent[requests.CreateBikeEvent](request)
+	errValidator := validators.BikeEvent(request)
 	if errValidator != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": errValidator,
@@ -25,9 +27,25 @@ func (bikeEvent *BikeEvent) CreateBikeEvent(ctx *fiber.Ctx) error {
 		return nil
 	}
 
+	userRole := ctx.Locals("user_role").(string)
+	uintOrganizerId, err := strconv.ParseUint(ctx.Locals("user_id").(string), 10, 64)
+	if err != nil {
+		ctx.Status(500).JSON(fiber.Map{
+			"message": "internal error",
+		})
+	}
+	organizerId := uint(uintOrganizerId)
+
+	if request.Organizer != organizerId && userRole != "admin" {
+		ctx.Status(403).JSON(fiber.Map{
+			"message": "you don't have permission to create this bike event on behalf of another user",
+		})
+		return nil
+	}
+
 	domain := domains.InitCreate(request.Name, request.StartPlace, request.AditionalInformation, request.StartDate, request.StartDateRegistration, request.EndDateRegistration, request.Organizer, request.ParticipantsLimit)
 
-	_, err := bikeEvent.service.CreateBikeEvent(*domain)
+	_, err = bikeEvent.service.CreateBikeEvent(*domain)
 	if err != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": err.Error(),
