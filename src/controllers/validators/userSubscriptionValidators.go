0a5eea870a9744d049a1864d72de3dc5825147cd
matheusgb <matheusgbr98@gmail.com>
2024-04-03 18:45:16 -0300

    refactor: rename UserSubscription struct request in validator file

--- src/controllers/validators/userSubscriptionValidators.go
+++ src/controllers/validators/userSubscriptionValidators.go
@@ -10,7 +10,7 @@ var mapUserSubscriptionRequestErrors = map[string]string{
 	"user_id":       "invalid user id, it must be a number greater than 0",
 }
 
-func UserSubscription(request requests.CreateUserSubscription) map[string]string {
+func UserSubscription(request requests.UserSubscription) map[string]string {
 	err := Validator.Struct(request)
 	if err != nil {
 		errors := make(map[string]string)
