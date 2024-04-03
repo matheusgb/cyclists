8604a09d6e9494e1d6300ea0bce3a0b77a414e78
matheusgb <matheusgbr98@gmail.com>
2024-04-03 13:37:47 -0300

    feat: create CheckUserIsInEvent function

---
+++ src/models/repositories/userSubscription/checkUserIsInEvent.go
@@ -0,0 +1,18 @@
+package repositories
+
+import (
+	"fmt"
+
+	domains "github.com/matheusgb/cyclists/src/models/domains/userSubscription"
+	"github.com/matheusgb/cyclists/src/models/repositories/entities"
+)
+
+func (userSubscription *UserSubscription) CheckUserIsInEvent(domain domains.UserSubscription) error {
+	err := userSubscription.database.Where(domain).First(&entities.UserSubscription{}).Error
+	if err == nil {
+		return fmt.Errorf("user already subscribed to this event")
+	} else if err.Error() != "record not found" {
+		return err
+	}
+	return nil
+}
