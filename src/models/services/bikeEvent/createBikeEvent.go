f6e0c63b75d7f798f0e262a3abf2824030a645be
matheusgb <matheusgbr98@gmail.com>
2024-04-03 13:39:18 -0300

    feat: create date validations in CreateBikeEvent

--- src/models/services/bikeEvent/createBikeEvent.go
+++ src/models/services/bikeEvent/createBikeEvent.go
@@ -1,10 +1,25 @@
 package services
 
 import (
+	"fmt"
+	"time"
+
 	domains "github.com/matheusgb/cyclists/src/models/domains/bikeEvent"
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
 )
 
 func (bikeEvent *BikeEvent) CreateBikeEvent(domain domains.BikeEvent) (entities.BikeEvent, error) {
+	if domain.StartDate.Before(domain.EndDateRegistration) {
+		return entities.BikeEvent{}, fmt.Errorf("the event start date should be after the end date")
+	}
+
+	if domain.StartDate.Before(time.Now()) || domain.EndDateRegistration.Before(time.Now()) || domain.StartDateRegistration.Before(time.Now()) {
+		return entities.BikeEvent{}, fmt.Errorf("the date registration should be in the future")
+	}
+
+	if domain.StartDateRegistration.After(domain.EndDateRegistration) || domain.StartDateRegistration.Equal(domain.EndDateRegistration) {
+		return entities.BikeEvent{}, fmt.Errorf("the start date registration should be before the end date registration")
+	}
+
 	return bikeEvent.repository.CreateBikeEvent(domain)
 }
