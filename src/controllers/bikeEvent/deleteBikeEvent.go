4293b30a982fd1ae3974fcc0f0657997ae961a7b
matheusgb <matheusgbr98@gmail.com>
2024-04-03 18:41:31 -0300

    refactor: adjust error message in DeleteBikeEvent

--- src/controllers/bikeEvent/deleteBikeEvent.go
+++ src/controllers/bikeEvent/deleteBikeEvent.go
@@ -19,8 +19,8 @@ func (bikeEvent *BikeEvent) DeleteBikeEvent(ctx *fiber.Ctx) error {
 	userRole := ctx.Locals("user_role").(string)
 	uintOrganizerId, err := strconv.ParseUint(ctx.Locals("user_id").(string), 10, 64)
 	if err != nil {
-		ctx.Status(400).JSON(fiber.Map{
-			"message": "invalid context user ID",
+		ctx.Status(500).JSON(fiber.Map{
+			"message": "internal error",
 		})
 	}
 	organizerId := uint(uintOrganizerId)
