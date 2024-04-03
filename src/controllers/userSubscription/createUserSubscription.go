60a7335606077719b697e362b7c5ec297219a590
matheusgb <matheusgbr98@gmail.com>
2024-04-03 18:44:21 -0300

    feat: create user validator in CreateUserSubscription

--- src/controllers/userSubscription/createUserSubscription.go
+++ src/controllers/userSubscription/createUserSubscription.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	requests "github.com/matheusgb/cyclists/src/controllers/requests/userSubscription"
 	"github.com/matheusgb/cyclists/src/controllers/validators"
@@ -8,7 +10,7 @@ import (
 )
 
 func (UserSubscription *UserSubscription) CreateUserSubscription(ctx *fiber.Ctx) error {
-	var request requests.CreateUserSubscription
+	var request requests.UserSubscription
 
 	if err := ctx.BodyParser(&request); err != nil {
 		ctx.Status(400).JSON(fiber.Map{
@@ -25,9 +27,25 @@ func (UserSubscription *UserSubscription) CreateUserSubscription(ctx *fiber.Ctx)
 		return nil
 	}
 
-	domain := domains.InitCreate(request.BikeEventID, request.UserID)
+	userRole := ctx.Locals("user_role").(string)
+	uintUserId, err := strconv.ParseUint(ctx.Locals("user_id").(string), 10, 64)
+	if err != nil {
+		ctx.Status(500).JSON(fiber.Map{
+			"message": "internal error",
+		})
+	}
+	userId := uint(uintUserId)
 
-	_, err := UserSubscription.service.CreateUserSubscription(*domain)
+	if request.UserID != userId && userRole != "admin" {
+		ctx.Status(403).JSON(fiber.Map{
+			"message": "you don't have permission to create this user subscription on behalf of another user",
+		})
+		return nil
+	}
+
+	domain := domains.Init(request.BikeEventID, request.UserID)
+
+	_, err = UserSubscription.service.CreateUserSubscription(*domain)
 	if err != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": err.Error(),
