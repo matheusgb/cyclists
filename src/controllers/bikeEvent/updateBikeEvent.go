b9db16bf764ca58ccce798518e0948014d0f4a81
matheusgb <matheusgbr98@gmail.com>
2024-04-03 18:41:46 -0300

    refactor: adjusts error message in UpdateBikeEvent

--- src/controllers/bikeEvent/updateBikeEvent.go
+++ src/controllers/bikeEvent/updateBikeEvent.go
@@ -23,13 +23,13 @@ func (bikeEvent *BikeEvent) UpdateBikeEvent(ctx *fiber.Ctx) error {
 	userRole := ctx.Locals("user_role").(string)
 	uintOrganizerId, err := strconv.ParseUint(ctx.Locals("user_id").(string), 10, 64)
 	if err != nil {
-		ctx.Status(400).JSON(fiber.Map{
-			"message": "invalid context user ID",
+		ctx.Status(500).JSON(fiber.Map{
+			"message": "internal error",
 		})
 	}
 	organizerId := uint(uintOrganizerId)
 
-	errValidator := validators.BikeEvent[requests.UpdateBikeEvent](request)
+	errValidator := validators.BikeEvent(request)
 	if errValidator != nil {
 		ctx.Status(400).JSON(fiber.Map{
 			"message": errValidator,
