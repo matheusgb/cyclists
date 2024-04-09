8023d78a26c9da1a7c8cc42d3786a9c221d14812
matheusgb <matheusgbr98@gmail.com>
2024-04-09 15:38:14 -0300

    feat: add pagination in GetAllUsers repository

--- src/models/repositories/user/getAllUsers.go
+++ src/models/repositories/user/getAllUsers.go
@@ -1,16 +1,18 @@
 package repositories
 
 import (
+	domainsP "github.com/matheusgb/cyclists/src/models/domains/pagination"
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
 )
 
-func (user *User) GetAllUsers() ([]entities.User, error) {
+func (user *User) GetAllUsers(pagination *domainsP.Pagination) (*domainsP.Pagination, error) {
 	var entities []entities.User
 
-	result := user.database.Select("id, name, email, created_at, updated_at").Preload("BikeEvents").Find(&entities)
+	result := user.database.Scopes(domainsP.Paginate(&entities, pagination, user.database)).Select("id, name, email, created_at, updated_at").Preload("BikeEvents").Find(&entities)
 	if result.Error != nil {
-		return entities, result.Error
+		return pagination, result.Error
 	}
+	pagination.Rows = entities
 
-	return entities, nil
+	return pagination, nil
 }
