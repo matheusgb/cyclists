8e6eb400930acd89a26f3accc6b86aa09ffcea21
matheusgb <matheusgbr98@gmail.com>
2024-04-09 15:37:57 -0300

    feat: add pagination in GetAllBikeEvents repository

--- src/models/repositories/bikeEvent/getAllBikeEvents.go
+++ src/models/repositories/bikeEvent/getAllBikeEvents.go
@@ -1,16 +1,18 @@
 package repositories
 
 import (
+	domainsP "github.com/matheusgb/cyclists/src/models/domains/pagination"
 	"github.com/matheusgb/cyclists/src/models/repositories/entities"
 )
 
-func (bikeEvent *BikeEvent) GetAllBikeEvents() ([]entities.BikeEvent, error) {
+func (bikeEvent *BikeEvent) GetAllBikeEvents(pagination *domainsP.Pagination) (*domainsP.Pagination, error) {
 	var entities []entities.BikeEvent
 
-	result := bikeEvent.database.Joins("User").Preload("Participants").Find(&entities)
+	result := bikeEvent.database.Scopes(domainsP.Paginate(&entities, pagination, bikeEvent.database)).Joins("User").Preload("Participants").Find(&entities)
 	if result.Error != nil {
-		return entities, result.Error
+		return pagination, result.Error
 	}
+	pagination.Rows = entities
 
-	return entities, nil
+	return pagination, nil
 }
