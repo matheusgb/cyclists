ea74e8324dc1dd4a2ca2cb7282314eafdce66b8e
matheusgb <matheusgbr98@gmail.com>
2024-05-05 21:56:55 -0300

    test: create DeleteUserRepository tests

---
+++ src/tests/repositories/user/deleteUser_test.go
@@ -0,0 +1,53 @@
+package tests
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	domains "github.com/matheusgb/cyclists/src/models/domains/user"
+	repositories "github.com/matheusgb/cyclists/src/models/repositories/user"
+	"github.com/matheusgb/cyclists/src/tests"
+	"github.com/stretchr/testify/assert"
+)
+
+func initDeleteMockedDomain() *domains.User {
+	request := map[string]string{
+		"ID": "1",
+	}
+
+	domain := domains.InitID(request["ID"])
+	return domain
+}
+
+func TestDeleteUserRepositorySuccess(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initDeleteMockedDomain()
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE`)).
+		WithArgs(sqlmock.AnyArg(), domain.ID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	repository := repositories.Init(db)
+	_, err := repository.DeleteUser(*domain)
+
+	assert.NoError(t, err)
+}
+
+func TestDeleteUserRepositoryError(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initDeleteMockedDomain()
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE`)).
+		WithArgs(sqlmock.AnyArg(), domain.ID).
+		WillReturnError(db.Error)
+	mock.ExpectCommit()
+
+	repository := repositories.Init(db)
+	_, err := repository.DeleteUser(*domain)
+
+	assert.Error(t, err)
+}
