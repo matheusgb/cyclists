c0ff1b6f713d53270c0fdafd356e51d80d138eb1
matheusgb <matheusgbr98@gmail.com>
2024-05-06 19:32:01 -0300

    test: create TestFindUserByEmailRepository tests

---
+++ src/tests/repositories/user/findUserByEmail_test.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	domains "github.com/matheusgb/cyclists/src/models/domains/user"
+	repositories "github.com/matheusgb/cyclists/src/models/repositories/user"
+	"github.com/matheusgb/cyclists/src/tests"
+	"github.com/stretchr/testify/assert"
+)
+
+func initFindByEmailMockedDomain() *domains.User {
+	request := map[string]string{
+		"email": "test@mail.com",
+	}
+
+	domain := domains.InitSendPasswordResetEmail(request["email"])
+	return domain
+}
+
+func TestFindUserByEmailRepositorySuccess(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initFindByEmailMockedDomain()
+
+	mock.ExpectQuery("SELECT").
+		WithArgs(domain.Email).
+		WillReturnRows(sqlmock.NewRows([]string{"email"}).
+			AddRow(domain.Email))
+	mock.ExpectBegin()
+
+	repository := repositories.Init(db)
+	user, err := repository.FindUserByEmail(*domain)
+
+	assert.NoError(t, err)
+	assert.Equal(t, domain.Email, user.Email)
+}
+
+func TestFindUserByEmailRepositoryError(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initFindByEmailMockedDomain()
+
+	mock.ExpectQuery("SELECT").
+		WithArgs(domain.Email).
+		WillReturnError(db.Error)
+	mock.ExpectRollback()
+
+	repository := repositories.Init(db)
+	_, err := repository.FindUserByEmail(*domain)
+
+	assert.Error(t, err)
+}
