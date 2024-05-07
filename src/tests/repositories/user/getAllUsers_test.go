fddb52757f89fccbe21617900e8d77bdef0f16dc
matheusgb <matheusgbr98@gmail.com>
2024-05-06 22:09:38 -0300

    test: create GetAllUsersRepository tests

---
+++ src/tests/repositories/user/getAllUsers_test.go
@@ -0,0 +1,93 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	domains "github.com/matheusgb/cyclists/src/models/domains/pagination"
+	"github.com/matheusgb/cyclists/src/models/repositories/entities"
+	repositories "github.com/matheusgb/cyclists/src/models/repositories/user"
+	"github.com/matheusgb/cyclists/src/tests"
+	"github.com/stretchr/testify/assert"
+)
+
+func initGetAllUsersPaginationMockedDomain() *domains.Pagination {
+	domain := &domains.Pagination{
+		Page:  0,
+		Limit: 0,
+		Sort:  "",
+	}
+
+	return domain
+}
+
+func TestGetAllUsersRepositorySuccessWithoutParam(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initGetAllUsersPaginationMockedDomain()
+
+	mock.ExpectQuery("SELECT").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).
+			AddRow(1).AddRow(2))
+
+	mock.ExpectQuery("SELECT").
+		WillReturnRows(sqlmock.NewRows([]string{"email"}).
+			AddRow("test@mail.com").AddRow("test2@mail.com"))
+
+	repository := repositories.Init(db)
+	user, err := repository.GetAllUsers(domain, "")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(user.Rows.([]entities.User)))
+	assert.Equal(t, "test@mail.com", user.Rows.([]entities.User)[0].Email)
+	assert.Equal(t, "test2@mail.com", user.Rows.([]entities.User)[1].Email)
+	assert.Equal(t, int64(2), user.TotalRows)
+	assert.Equal(t, 1, user.TotalPages)
+	assert.Equal(t, 1, user.Page)
+	assert.Equal(t, 10, user.Limit)
+	assert.Equal(t, "Id desc", user.Sort)
+}
+
+func TestGetAllUsersRepositorySuccessWithParam(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initGetAllUsersPaginationMockedDomain()
+
+	mock.ExpectQuery("SELECT").
+		WithArgs("%test@mail.com%").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).
+			AddRow(1))
+
+	mock.ExpectQuery("SELECT").
+		WithArgs("%test@mail.com%").
+		WillReturnRows(sqlmock.NewRows([]string{"email"}).
+			AddRow("test@mail.com"))
+
+	repository := repositories.Init(db)
+	user, err := repository.GetAllUsers(domain, "test@mail.com")
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(user.Rows.([]entities.User)))
+	assert.Equal(t, "test@mail.com", user.Rows.([]entities.User)[0].Email)
+	assert.Equal(t, int64(1), user.TotalRows)
+	assert.Equal(t, 1, user.TotalPages)
+	assert.Equal(t, 1, user.Page)
+	assert.Equal(t, 10, user.Limit)
+	assert.Equal(t, "Id desc", user.Sort)
+}
+
+func TestGetAllUsersRepositoryError(t *testing.T) {
+	db, mock := tests.MockDatabase()
+	domain := initGetAllUsersPaginationMockedDomain()
+
+	mock.ExpectQuery("SELECT").
+		WithArgs("%test%").
+		WillReturnError(db.Error)
+
+	mock.ExpectQuery("SELECT").
+		WithArgs("%test%").
+		WillReturnError(db.Error)
+
+	repository := repositories.Init(db)
+	_, err := repository.GetAllUsers(domain, "test")
+
+	assert.Error(t, err)
+}
