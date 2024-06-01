f3896bc647e765140d53c93ff35aa7c2f1aecacb
matheusgb <matheusgbr98@gmail.com>
2024-06-01 19:12:34 -0300

    test: create GetUserService tests

---
+++ src/tests/services/user/getUser_test.go
@@ -0,0 +1,41 @@
+package tests
+
+import (
+	"testing"
+
+	domains "github.com/matheusgb/cyclists/src/models/domains/user"
+	"github.com/matheusgb/cyclists/src/models/repositories/entities"
+	services "github.com/matheusgb/cyclists/src/models/services/user"
+	"github.com/matheusgb/cyclists/src/tests/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"gorm.io/gorm"
+)
+
+func TestGetUserService(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	domain := domains.InitID("1")
+	repository := mocks.NewMockIUser(ctrl)
+	service := services.Init(repository)
+
+	t.Run("Success", func(t *testing.T) {
+		entity := entities.User{
+			Model: gorm.Model{
+				ID: 1,
+			},
+			Name:     "Test",
+			Email:    "test@mail.com",
+			Password: domain.Password,
+		}
+
+		repository.EXPECT().GetUser(*domain).Return(entity, nil)
+		entity, err := service.GetUser(*domain)
+
+		assert.NoError(t, err)
+		assert.Equal(t, uint(1), entity.ID)
+		assert.Equal(t, "Test", entity.Name)
+		assert.Equal(t, "test@mail.com", entity.Email)
+	})
+}
